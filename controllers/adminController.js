44fd378f3573bb61de0aae529e9fbd244e590d0b
Mohammad Ali <itsproali@gmail.com>
2022-10-13 23:47:14 +0600

    ⚡️ Admin Routes Added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
+const Job = require("../models/Job");
 
+// Get All Candidates
 exports.getAllCandidates = async (req, res, next) => {
   try {
     const data = await User.find(
@@ -8,6 +10,62 @@ exports.getAllCandidates = async (req, res, next) => {
       { runValidators: true }
     );
     res.status(200).send({ success: true, totalCandidate: data.length, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get a specific Candidate Details by id
+exports.getCandidateDetails = async (req, res, next) => {
+  try {
+    const candidateId = req.params.id;
+    const data = await User.findOne(
+      { _id: candidateId },
+      { password: 0 }
+    ).populate("appliedJob");
+
+    data.appliedJob.candidates = undefined;
+
+    res.status(200).send({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get All Hiring Manger
+exports.getAllManager = async (req, res, next) => {
+  try {
+    const data = await User.find(
+      { role: "Hiring Manager" },
+      { password: 0 },
+      { runValidators: true }
+    );
+    res.status(200).send({ success: true, totalManager: data.length, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Top 10 paid Jobs
+exports.getTopPaidJobs = async (req, res, next) => {
+  try {
+    const data = await Job.find({}, null, { sort: { salary: -1 }, limit: 10 });
+
+    res.status(200).send({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Top 5 applied Jobs
+exports.getTopAppliedJobs = async (req, res, next) => {
+  try {
+    const data = await Job.find({}, null, {
+      sort: { applyCont: -1 },
+      limit: 5,
+    });
+
+    res.status(200).send({ success: true, data });
   } catch (error) {
     next(error);
   }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
 const verifyToken = require("../middleware/verifyToken");
 const authorization = require("../middleware/authorization");
-const { getAllCandidates } = require("../controllers/adminController");
+const {
+  getAllCandidates,
+  getCandidateDetails,
+  getAllManager,
+  getTopPaidJobs,
+  getTopAppliedJobs,
+} = require("../controllers/adminController");
 
 // Verify Admin
 router.use(verifyToken);
 router.use(authorization("Admin"));
 
 router.get("/candidates", getAllCandidates);
+router.get("/candidate/:id", getCandidateDetails);
+router.get("/managers", getAllManager);
+router.get("/top-paid-jobs", getTopPaidJobs);
+router.get("/top-applied-jobs", getTopAppliedJobs);
 
 module.exports = router;
